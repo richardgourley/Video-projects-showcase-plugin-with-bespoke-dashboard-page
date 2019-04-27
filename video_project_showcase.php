ddd2f61cfc10eb5048df2cd783ccb2016bddf280
unknown <codeleap@outlook.com>
2019-04-27 21:14:02 +0200

    Main plugin file added.

---
+++ video_project_showcase.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+* Plugin Name: Video Projects Showcase
+* Plugin URI: http://wprevs.com
+* Description: A plugin to allow a video production company to showcase their projects in a Wordpress site 
+via a bespoke dashboard page. 
+* Author: Richard Gourley
+* Version: 1.0.0
+* Author URI: http://wprevs.com
+* License: GPLv2 or later
+*/
+
+//check abspath
+if(!defined( 'ABSPATH' )) exit;
+
+$vps_init = new VideoProjectShowcaseInit();
