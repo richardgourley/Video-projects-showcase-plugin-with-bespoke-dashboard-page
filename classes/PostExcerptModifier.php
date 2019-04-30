4aa4e12424177f500dfa24161da92899079b88e8
unknown <codeleap@outlook.com>
2019-04-30 16:28:37 +0200

    Added new filter for get_the_excerpt changing length of excerpt to 40 for video projects post types.

--- classes/PostExcerptModifier.php
+++ classes/PostExcerptModifier.php
@@ -1,6 +1,6 @@
 <?php
 
-class VpsPostExcerptInit{
+class VpsPostExcerptModifier{
     public function __construct(){
         /* remove the default filter */
         //remove_filter('get_the_excerpt', 'wp_trim_excerpt');
@@ -9,24 +9,23 @@ class VpsPostExcerptInit{
         //add_filter('get_the_excerpt', array( $this, 'vps_wp_trim_excerpt' ));
     }
 
-    function vps_wp_trim_excerpt($text) { // Fakes an excerpt if needed
-    global $post;
-    //var_dump($post);
+    function vps_wp_trim_excerpt($text) { 
+        global $post;
     
-    if ( '' == $text ) {
-        $text = get_the_content('');
-        $text = apply_filters('the_content', $text);
-        $text = str_replace('\]\]\>', ']]&gt;', $text);
-        //$text = strip_tags($text);
-        $excerpt_length = 55;
-        $words = explode(' ', $text, $excerpt_length + 1);
-        if (count($words)> $excerpt_length) {
-            array_pop($words);
-            array_push($words, '[...]');
-            $text = implode(' ', $words);
+        if ( '' == $text ) {
+            $text = get_the_content('');
+            $text = apply_filters('the_content', $text);
+            $text = str_replace('\]\]\>', ']]&gt;', $text);
+            $text = strip_tags($text);
+            $excerpt_length = $post->post_type == 'video_project' ? 35 : 55;
+            $words = explode(' ', $text, $excerpt_length + 1);
+            if (count($words)> $excerpt_length) {
+                array_pop($words);
+                array_push($words, '[...]');
+                $text = implode(' ', $words);
+            }
         }
+        return $text;
     }
-    return $text;
-}
 
 }
