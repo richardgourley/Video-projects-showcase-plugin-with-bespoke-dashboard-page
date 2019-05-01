26897c4456f39a948184414450165a1bf2e218a0
unknown <codeleap@outlook.com>
2019-05-01 17:39:11 +0200

    JS admin folder and file added.

---
+++ js/admin-page-js.js
@@ -0,0 +1,36 @@
+//alert("Testing JS file enqueued sucsessfully");
+/*jQuery(document).ready(function($){
+
+  var mediaUploader;
+
+  $('#upload_button').click(function(e) {
+    e.preventDefault();
+    // If the uploader object has already been created, reopen the dialog
+      if (mediaUploader) {
+      mediaUploader.open();
+      return;
+    }
+    // Extend the wp.media object
+    mediaUploader = wp.media.frames.file_frame = wp.media({
+      title: 'Choose Image',
+      button: {
+      text: 'Choose Image'
+    }, multiple: false });
+
+    // When a file is selected, grab the URL and set it as the text field's value
+    mediaUploader.on('select', function() {
+      attachment = mediaUploader.state().get('selection').first().toJSON();
+      $('#select_image').val(attachment.url);
+    });
+    // Open the uploader dialog
+    mediaUploader.open();
+  });
+
+});*/
+
+alert("TESTING VIDEO PROJECT SHOWCASE JS!!!");
+
+
+
+
+
---
+++ js/admin-page.js
@@ -0,0 +1,6 @@
+//JS code for form handling and cpt display page to be added.
+
+
+
+
+
