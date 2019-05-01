2a44e86a2dc9f0e11d8ea28b66e8b442d7a97201
unknown <codeleap@outlook.com>
2019-05-01 17:57:54 +0200

    Custom post type, admin page and taxonomy classes added and instantiated in main plugin file.

--- video_project_showcase.php
+++ video_project_showcase.php
@@ -5,7 +5,7 @@
 * Plugin URI: http://wprevs.com
 * Description: A plugin to allow a video production company to showcase their projects in a Wordpress site 
 via a bespoke dashboard page. 
-* Author: Richard Gourley
+* Author: wprevs.com
 * Version: 1.0.0
 * Author URI: http://wprevs.com
 * License: GPLv2 or later
@@ -15,4 +15,20 @@ via a bespoke dashboard page.
 if(!defined( 'ABSPATH' )) exit;
 
 require dirname( __FILE__ ) . '/classes/AdminPageInitializer.php';
+require dirname( __FILE__ ) . '/classes/ScriptsInitializer.php';
+require dirname( __FILE__ ) . '/classes/CustomPostTypeInitializer.php';
+require dirname( __FILE__ ) . '/classes/TaxonomiesInitializer.php';
+
+
+//initializes admin page in dashboard
 $vps_admin_page_initializer = new VpsAdminPageInitializer();
+
+//initializes css and js scripts
+$vps_scripts_initializer = new VpsScriptsInitializer();
+
+//initializes custom post type
+$vps_custom_post_type_initializer = new VpsCustomPostTypeInitializer();
+
+
+//initializes taxonomies
+$vps_taxonomies_initializer = new VpsTaxonomiesInitializer();
