e65b39431575971aa51a2924623e5f0c7f712bfe
unknown <codeleap@outlook.com>
2019-05-01 17:55:22 +0200

    Country and Video category taxonomies added to class.

--- classes/TaxonomiesInitializer.php
+++ classes/TaxonomiesInitializer.php
@@ -1,18 +1,64 @@
 <?php
 
-class VpsTaxonomiesInit{
+class VpsTaxonomiesInitializer{
     public function __construct(){
-        //add_action( 'init', array( $this, 'add_video_project_taxonomies' ));
+        add_action( 'init', array( $this, 'add_video_project_taxonomies' ));
     }
 
     public function add_video_project_taxonomies(){
-        $labels_country = array();
-        $args_country = array();
-        register_taxonomy( 'country', $args_country);
-        
+        $labels_country = array(
+            "name" => __( "Country" ),
+             "singular_name" => __( "Country" ),
+        );
+
+        $args_country = array(
+            "label" => __( "Country" ),
+            "labels" => $labels_country,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => false,
+            "show_ui" => true,
+            "show_in_menu" => false,
+            "show_in_nav_menus" => true,
+            "query_var" => true,
+            "rewrite" => array( 'slug' => 'countries', 'with_front' => true, ),
+            "show_admin_column" => false,
+            "show_in_rest" => true,
+            "rest_base" => "countries",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "show_in_quick_edit" => false,
+        );
+
+        $labels_video_category = array(
+            "name" => __( "Video category" ),
+             "singular_name" => __( "Video category" ),
+        );
+
+        $args_video_category = array(
+            "label" => __( "Video category" ),
+            "labels" => $labels_video_category,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => false,
+            "show_ui" => true,
+            "show_in_menu" => false,
+            "show_in_nav_menus" => true,
+            "query_var" => true,
+            "rewrite" => array( 'slug' => 'video_categories', 'with_front' => true, ),
+            "show_admin_column" => false,
+            "show_in_rest" => true,
+            "rest_base" => "video_category",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "show_in_quick_edit" => false,
+        );
+
+        register_taxonomy( 'country', array( "video_project" ), $args_country );
+        register_taxonomy( 'video_category', array( "video_project" ), $args_video_category );
+        /*
         $labels_video_type = array();
         $args_video_type = array();
         register_taxonomy( 'video_type', $args_video_type);
+        */
     }
 
 }
