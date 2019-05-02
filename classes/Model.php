f798d829cce07b5d6f9f08cc3a7a0b81af192717
unknown <codeleap@outlook.com>
2019-05-02 18:40:11 +0200

    Instruction added to functions in Model file.

--- classes/Model.php
+++ classes/Model.php
@@ -3,11 +3,11 @@
 class VpsModel{
     
     public function __construct(){
-        
+        //constructor logic
     }
 
     public function insert_video_project(){
-
+        //handle insert video project form submission here
     }
 
 }
