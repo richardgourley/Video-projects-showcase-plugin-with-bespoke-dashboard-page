2ae975607dd09d1fb05d9433fe37ac806db2b1fa
unknown <codeleap@outlook.com>
2019-05-02 18:39:03 +0200

    Updated scripts file to enqueue js and css.

--- classes/ScriptsInitializer.php
+++ classes/ScriptsInitializer.php
@@ -3,16 +3,17 @@
 class VpsScriptsInitializer{
     public function __construct(){
         //enqueue scripts here
-        //add_action( 'init', array( $this, 'enqueue_css' ) );
-        //add_action( 'init', array( $this, 'enqueue_js' ) );
+        add_action( 'init', array( $this, 'enqueue_css' ) );
+        add_action( 'init', array( $this, 'enqueue_js' ) );
     }
 
     public function enqueue_css(){
-        
+        wp_enqueue_style( 'video_projects_showcase_css', plugins_url( 'css/styles.css', __DIR__) );
     }
 
     public function enqueue_js(){
-        
+        wp_enqueue_script( 'video_projects_showcase_js', plugins_url( 'js/admin-page.js', __DIR__),
+        array(), '1.0.0', true );
     }
 
 }
