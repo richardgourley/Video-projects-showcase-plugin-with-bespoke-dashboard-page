f30a067c9e43c8c71d7f0754354653916926b463
unknown <codeleap@outlook.com>
2019-05-03 17:10:01 +0200

    Date, duration and select image fields added.

--- admin_pages/add_video_project_form.php
+++ admin_pages/add_video_project_form.php
@@ -11,7 +11,7 @@
         		<p><small>Enter the project name or the name of the customer or client.</small></p>
         	</th>
         	<td>
-        		<input class="regular-text" id="title" name="title" type="text">
+        		<input class="regular-text" id="title" name="title" type="text" value="">
         	</td>
         </tr>
         <tr>
@@ -20,15 +20,35 @@
         		<p><small>Enter the location of the project.</small></p>
         	</th>
         	<td>
-        		<input class="regular-text" id="location" name="location" type="text">
+        		<input class="regular-text" id="location" name="location" type="text" value="">
         	</td>
         </tr>
         <tr>
         	<th>
-        		
+        		<label for="date">Date</label>
+        		<p><small>Select a date for the project.</small></p>
         	</th>
         	<td>
-        		
+        		<input class="regular-text" id="date" name="date" type="date" value="">
+        	</td>
+        </tr>
+        <tr>
+        	<th>
+        		<label for="date">Duration</label>
+        		<p><small>Enter a duration eg 2 weeks, 3 months.</small></p>
+        	</th>
+        	<td>
+        		<input class="regular-text" id="duration" name="duration" type="text" value="">
+        	</td>
+        </tr>
+        <tr>
+        	<th>
+        		<label for="image">Image</label>
+        		<p><small>Click the button to see and choose an image from the media library.</small></p>
+        	</th>
+        	<td>
+        		<input class="regular-text" id="image" name="image" type="text" value="">
+        		<input class="button insert_video_project_form_button" id="upload_insert_video_project" name="video_project_button" type="button" value="upload"/>
         	</td>
         </tr>
     </tbody>
