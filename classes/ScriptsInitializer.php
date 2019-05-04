1bb63d5c79b14a9b4555668c1bd8316e00607d27
unknown <codeleap@outlook.com>
2019-05-04 13:57:27 +0200

    Enqueue js hook modified to admin_enqueue_scripts instead of init.

--- classes/ScriptsInitializer.php
+++ classes/ScriptsInitializer.php
@@ -4,15 +4,15 @@ class VpsScriptsInitializer{
     public function __construct(){
         //enqueue scripts here
         add_action( 'init', array( $this, 'enqueue_css' ) );
-        add_action( 'init', array( $this, 'enqueue_js' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_js' ) );
     }
 
     public function enqueue_css(){
         wp_enqueue_style( 'video_projects_showcase_css', plugins_url( 'css/styles.css', __DIR__) );
     }
 
-    public function enqueue_js(){
-        wp_enqueue_script( 'video_projects_showcase_js', plugins_url( 'js/admin-page.js', __DIR__),
+    public function enqueue_admin_js(){
+        wp_enqueue_script( 'video_projects_showcase_js', plugins_url( 'js/admin-page.js', __DIR__ ),
         array(), '1.0.0', true );
     }
 
