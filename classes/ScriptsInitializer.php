deee5e036c5c74e24f698836dbbf0987cd09c7cd
unknown <codeleap@outlook.com>
2019-05-04 15:29:18 +0200

    Added enqueuing of admin-page-media.js

--- classes/ScriptsInitializer.php
+++ classes/ScriptsInitializer.php
@@ -12,8 +12,10 @@ class VpsScriptsInitializer{
     }
 
     public function enqueue_admin_js(){
-        wp_enqueue_script( 'video_projects_showcase_js', plugins_url( 'js/admin-page.js', __DIR__ ),
+        wp_enqueue_script( 'video_projects_admin_js', plugins_url( 'js/admin-page.js', __DIR__ ),
         array(), '1.0.0', true );
+        wp_enqueue_script( 'video_projects_admin_media_js', plugins_url( 'js/admin-page-media.js', __DIR__ ),
+        array('jquery'), '1.0.0', true);
     }
 
 }
