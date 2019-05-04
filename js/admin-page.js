ecc274a7d8007d3863ebfe53f04eedace1bfda07
unknown <codeleap@outlook.com>
2019-05-04 14:05:47 +0200

    Admin page JS variable names changed to camel case.

--- js/admin-page.js
+++ js/admin-page.js
@@ -10,16 +10,16 @@
 //div that displays country radio boxes
 var countries = document.getElementById("countries");
 //represents new_country radio button
-var add_new_country = document.getElementById("add_new_country");
+var addNewCountry = document.getElementById("addNewCountry");
 //label that shows or hides text input for new country
-var new_country_text_input = document.getElementById("new_country_text_input");
+var newCountryTextInput = document.getElementById("newCountryTextInput");
 
-add_new_country.onclick = function(){
-    new_country_text_input.innerHTML = '<input type="text" id="new_country" name="new_country" value="">';
+addNewCountry.onclick = function(){
+    newCountryTextInput.innerHTML = '<input type="text" id="new_country" name="new_country" value="">';
 }
 
 countries.onclick = function(){
-    new_country_text_input.innerHTML = '<input type="hidden" id="new_country" name="new_country" value="">';
+    newCountryTextInput.innerHTML = '<input type="hidden" id="new_country" name="new_country" value="">';
 }
 
 //=================================================
