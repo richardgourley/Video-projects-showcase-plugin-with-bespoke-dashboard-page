a6a7ce167a44604e40dbf0a9f23880faae6af591
unknown <codeleap@outlook.com>
2019-05-04 13:55:51 +0200

    Insert video project post form added to callback method.

--- classes/AdminPageInitializer.php
+++ classes/AdminPageInitializer.php
@@ -12,6 +12,21 @@ class VpsAdminPageInitializer{
     public function video_project_admin_page_callback(){
     	//displays main admin page in dashboard.
         require_once plugin_dir_path( __DIR__ ) . '/admin_pages/main.php';
+        require_once plugin_dir_path( __DIR__ ) . '/classes/Model.php';
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //loads add video project form
+        	if(isset($_POST['add_video_project_nonce']) && wp_verify_nonce($_POST['add_video_project_nonce'], 'add_video_project_action')){
+        		$model = new VpsModel();
+        		$model->display_video_project_form();
+        	}
+
+            //handles insert video project logic
+            if(isset($_POST['insert_video_project_form_nonce']) && wp_verify_nonce($_POST['insert_video_project_form_nonce'], 'insert_video_project_form_action')){
+                $model = new VpsModel();
+                $model->insert_video_project( );
+            }
+        }
     }
 
 }
