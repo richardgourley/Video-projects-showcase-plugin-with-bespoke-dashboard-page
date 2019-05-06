a6d6a3209e8377b6b15e6b23e853ad2aae0c4350
unknown <codeleap@outlook.com>
2019-05-06 19:05:45 +0200

    Input elements changed to lowercase and hyphen.

--- admin-pages/main.php
+++ admin-pages/main.php
@@ -1,10 +1,10 @@
-<div class="vps_main_page_div">
+<div class="vps-main-page-div">
 
-<div class="vps_main_page_greeting">
-<h1>Hello there <span class="vps_username_box"><?php echo esc_html(wp_get_current_user()->user_nicename); ?></span></h1>
+<div class="vps-main-page-greeting">
+<h1>Hello there <span class="vps-username-box"><?php echo esc_html(wp_get_current_user()->user_nicename); ?></span></h1>
 </div>
-<div class="vps_main_page_header">
-<h1 class="vps_main_page_header_font">Welcome to your bespoke admin page!</h1>
+<div class="vps-main-page-header">
+<h1 class="vps-main-page-header-font">Welcome to your bespoke admin page!</h1>
 </div>
 
 
@@ -12,28 +12,28 @@
 <div><p>Here, you can create, update, insert and manage all of your custom post types.</p></div>
 <div><p>We hope this page makes it very quick and very easy to manage the bespoke content for your site all in 1 place.</p></div>
 
-<div class="vps_menu_buttons_wrapper">
-<div class="vps_row">
-<div class="vps_column">
+<div class="vps-menu-buttons-wrapper">
+<div class="vps-row">
+<div class="vps-column">
 
 <h2>Add video project</h2>
-<form action="<?php echo esc_url( admin_url('admin.php?page=video_project')); ?>" method="post">
-   <input type="hidden" name="action" value="add_video_project">
-   <input type="hidden" name="add_video_project" value="1">
+<form action="<?php echo esc_url( admin_url('admin.php?page=video-project')); ?>" method="post">
+   <input type="hidden" name="action" value="add-video-project">
+   <input type="hidden" name="add-video-project" value="1">
    <input type="submit" name="submit" id="submit" value="Add Video Project">
-   <?php wp_nonce_field( 'add_video_project_action', 'add_video_project_nonce' ); ?>
+   <?php wp_nonce_field( 'add-video-project-action', 'add-video-project-nonce' ); ?>
 </form>
 
 </div><!----End column---->
 
-<div class="vps_column">
+<div class="vps-column">
 
 <h2>View all video projects</h2>
-<form action="<?php echo esc_url( admin_url('admin.php?page=video_project')); ?>" method="post">
-   <input type="hidden" name="action" value="view_all_video_projects">
-   <input type="hidden" name="view_all_video_projects" value="1">
+<form action="<?php echo esc_url( admin_url('admin.php?page=video-project')); ?>" method="post">
+   <input type="hidden" name="action" value="view-all-video-projects">
+   <input type="hidden" name="view-all-video-projects" value="1">
    <input type="submit" name="submit" id="submit" value="View all Video Projects">
-   <?php wp_nonce_field( 'view_all_video_projects_action', 'view_all_video_projects_nonce' ); ?>
+   <?php wp_nonce_field( 'view-all-video-projects-action', 'view-all-video-projects-nonce' ); ?>
 </form>
 
 </div><!----End column---->
