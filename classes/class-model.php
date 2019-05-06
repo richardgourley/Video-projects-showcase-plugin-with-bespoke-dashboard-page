b84c098d5913dd11ea53f53c2f2fca2511a50d56
unknown <codeleap@outlook.com>
2019-05-06 18:25:11 +0200

    Updated class name, updated link to admin-pages files.

--- classes/class-model.php
+++ classes/class-model.php
@@ -1,13 +1,19 @@
 <?php
 
-class VpsModel{
+class VPS_Model{
     
     public function __construct(){
         //constructor logic
     }
+    
+    public function display_video_project_form(){
+    	//displays form 
+    	require_once plugin_dir_path( __DIR__ ) . 'admin-pages/add-video-project-form.php';
+    }
 
-    public function insert_video_project(){
+    public function insert_video_project( ){
         //handle insert video project form submission here
+        
     }
 
 }
