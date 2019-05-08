51b76508d2c978a137d454f30cfa28070f9c79e9
unknown <codeleap@outlook.com>
2019-05-08 17:17:21 +0200

    field_is_blank method added to model class.

--- classes/class-vps-model.php
+++ classes/class-vps-model.php
@@ -13,7 +13,18 @@ class VPS_Model{
 
     public function insert_video_project( ){
         //handle insert video project form submission here
-        
+        $this->field_is_blank( $_POST );
+        /*var_dump(taxonomy_exists('country'));
+        var_dump(taxonomy_exists('video_category'));
+        global $wp_taxonomies;
+        var_dump($wp_taxonomies);
+        var_dump(term_exists('Uncategorized', 'category'));*/
+    }
+
+    private function field_is_blank( $post ){
+        foreach($post as $k => $v){
+            echo "<br>" . $k . " -> " . $v;
+        }
     }
 
 }
