89b7c4a46c734dd18ad68edf5114294abfdc45e7
unknown <codeleap@outlook.com>
2019-05-09 20:30:42 +0200

    Form validation and sanitization started.

--- classes/class-vps-model.php
+++ classes/class-vps-model.php
@@ -14,19 +14,51 @@ class VPS_Model{
     public function insert_video_project( ){
         //handle insert video project form submission here
         $message = $this->check_blank_fields( $_POST );
-        if($message == ''){
+
+        $video_category = sanitize_text_field($_POST['video-category']);
+        //video_category - add term to this post
+        $country = sanitize_text_field($_POST['country']);
+        $new_country = sanitize_text_field($_POST['new-country']);
+
+        if($country === 'add-new-country'){
+            //create new term for country taxonomy.
+            //check for irregular characters such as numbers, hyphens, @! etc.
+        }else{
+            //country is from existing terms... add as post taxonomy term
+        }
+
+        $title = sanitize_text_field($_POST['title']);
+        if($this->validate_date($_POST['date'])){
+            $date = $_POST['date'];
+        }else{
+            //handle invalid date
+        }
+        $duration = sanitize_text_field($_POST['duration']);
+        $video_url = sanitize_url($_POST['video-url']);
+        $video_project_image = sanitize_url($_POST['video-project-url']);
+        
+        var_dump($date);
+        var_dump($video_url);
+        var_dump($new_country);
+        var_dump($_POST['helloyou!']);
+        var_dump($video_category);
+        var_dump( $_POST );
+        var_dump( taxonomy_exists( 'video_category' ));
+        var_dump( taxonomy_exists( 'country' ));
+        /*if(!$message == ''){
             echo 'Now we will insert this post into the database';
             var_dump( $_POST );
             var_dump( taxonomy_exists('video_category' ));
         }else{
             echo 'There were some errors.<br>Some of your fields were left blank.<br>';
-        }
+        }*/
         /*var_dump(taxonomy_exists('country'));
         var_dump(taxonomy_exists('video_category'));
         global $wp_taxonomies;
         var_dump($wp_taxonomies);
         var_dump(term_exists('Uncategorized', 'category'));*/
 
+        
         //FORM HANDLING
         /*
         1. video_category - a) check term exists b) create term if not c) add to post?
@@ -49,5 +81,11 @@ class VPS_Model{
         }
         return $message;
     }
-
+    
+    private function validate_date( $date ){
+        $date_arr = explode( '-', $date);
+        var_dump($date_arr);
+        //MM DD YYYY
+        return wp_checkdate($date_arr[1], $date_arr[2], $date_arr[0], $date);
+    }
 }
