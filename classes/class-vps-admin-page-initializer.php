2020bb56cfd745ba4e7b48392e100de658b1416b
unknown <codeleap@outlook.com>
2019-05-18 17:26:03 +0200

    Create and update form handling added to replace insert form handling.

--- classes/class-vps-admin-page-initializer.php
+++ classes/class-vps-admin-page-initializer.php
@@ -16,25 +16,39 @@ class VPS_Admin_Page_Initializer{
         require_once plugin_dir_path( __DIR__ ) . '/classes/class-vps-model.php';
 
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            //loads add video project form
-        	if( isset($_POST['add-video-project-nonce'] ) 
-            && wp_verify_nonce($_POST[ 'add-video-project-nonce' ], 'add-video-project-action')){
+            //loads create video project form
+        	if( isset($_POST['create-video-project-form-nonce'] ) 
+            && wp_verify_nonce($_POST[ 'create-video-project-form-nonce' ], 'create-video-project-form-action')){
         		$model = new VPS_Model();
-        		$model->display_video_project_form();
+        		$model->create_video_project_form();
         	}
 
-            //handles insert video project form logic
-            if( isset($_POST['insert-video-project-form-nonce'] ) 
-            && wp_verify_nonce($_POST['insert-video-project-form-nonce'], 'insert-video-project-form-action')){
+            //when create form is completed, pass $_POST array to be handled by create_video_project
+            if( isset($_POST['create-video-project-nonce'] ) 
+            && wp_verify_nonce($_POST['create-video-project-nonce'], 'create-video-project-action')){
                 $model = new VPS_Model();
-                $model->insert_video_project( );
+                $model->create_video_project( );
             }
 
-            //displays all video projects with edit, update buttons
+            //displays all video projects with update buttons
             if(isset( $_POST['view-all-video-projects-nonce'] ) 
             && wp_verify_nonce($_POST[ 'view-all-video-projects-nonce' ], 'view-all-video-projects-action')){
                 $model = new VPS_Model();
                 $model->view_all_video_projects();
+            }
+
+            //when UPDATE is clicked for a video project, we load update video project form
+            if(isset( $_POST['update-video-project-form-nonce'] ) 
+            && wp_verify_nonce($_POST[ 'update-video-project-form-nonce' ], 'update-video-project-form-action')){
+                $model = new VPS_Model();
+                $model->update_video_project_form();
+            }
+
+            //when update form is completed, pass $_POST array to be handled by update video project
+            if(isset( $_POST['update-video-project-nonce'] ) 
+            && wp_verify_nonce($_POST[ 'update-video-project-nonce' ], 'update-video-project-action')){
+                $model = new VPS_Model();
+                $model->update_video_project();
             }
         }
     }
