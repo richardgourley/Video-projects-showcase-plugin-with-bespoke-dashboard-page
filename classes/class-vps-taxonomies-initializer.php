c313a36ac03e22ca0a6d45a2aa8e1e3321cee58e
unknown <codeleap@outlook.com>
2019-05-20 21:45:35 +0200

    Register taxonomy labels and args updated to consistent naming convention.

--- classes/class-vps-taxonomies-initializer.php
+++ classes/class-vps-taxonomies-initializer.php
@@ -3,24 +3,18 @@
 class VPS_Taxonomies_Initializer{
     public function __construct(){
         add_action( 'init', array( $this, 'add_video_project_taxonomies' ));
-        //if( !term_exists())
-        /*
-        if( !term_exists( 'spanish' )){
-            wp_insert_term( 'Spanish', 'video_project_language' );
-        }*/
-        
         
     }
 
     public function add_video_project_taxonomies(){
-        $labels_country = array(
-            "name" => __( "Country" ),
-             "singular_name" => __( "Country" ),
+        $labels_video_project_country = array(
+            "name" => __( "Video Project Country" ),
+             "singular_name" => __( "Video Project Country" ),
         );
 
-        $args_country = array(
-            "label" => __( "Country" ),
-            "labels" => $labels_country,
+        $args_video_project_country = array(
+            "label" => __( "Video Project Country" ),
+            "labels" => $labels_video_project_country,
             "public" => true,
             "publicly_queryable" => true,
             "hierarchical" => false,
@@ -28,22 +22,22 @@ class VPS_Taxonomies_Initializer{
             "show_in_menu" => false,
             "show_in_nav_menus" => true,
             "query_var" => true,
-            "rewrite" => array( 'slug' => 'countries', 'with_front' => true, ),
+            "rewrite" => array( 'slug' => 'video_project_countries', 'with_front' => true, ),
             "show_admin_column" => false,
             "show_in_rest" => true,
-            "rest_base" => "countries",
+            "rest_base" => "video_project_country",
             "rest_controller_class" => "WP_REST_Terms_Controller",
             "show_in_quick_edit" => false,
         );
 
-        $labels_video_category = array(
-            "name" => __( "Video category" ),
-             "singular_name" => __( "Video category" ),
+        $labels_video_project_category = array(
+            "name" => __( "Video Project Category" ),
+             "singular_name" => __( "Video Project Category" ),
         );
 
-        $args_video_category = array(
-            "label" => __( "Video category" ),
-            "labels" => $labels_video_category,
+        $args_video_project_category = array(
+            "label" => __( "Video Project Category" ),
+            "labels" => $labels_video_project_category,
             "public" => true,
             "publicly_queryable" => true,
             "hierarchical" => false,
@@ -51,21 +45,21 @@ class VPS_Taxonomies_Initializer{
             "show_in_menu" => false,
             "show_in_nav_menus" => true,
             "query_var" => true,
-            "rewrite" => array( 'slug' => 'video_categories', 'with_front' => true, ),
+            "rewrite" => array( 'slug' => 'video_project_categories', 'with_front' => true, ),
             "show_admin_column" => false,
             "show_in_rest" => true,
-            "rest_base" => "video_category",
+            "rest_base" => "video_project_category",
             "rest_controller_class" => "WP_REST_Terms_Controller",
             "show_in_quick_edit" => false,
         );
 
         $labels_video_project_language = array(
-            "name" => __( "Language" ),
-            "singular_name" => __( "Language" ),
+            "name" => __( "Video Project Language" ),
+            "singular_name" => __( "Video Project Language" ),
         );
 
         $args_video_project_language = array(
-            "label" => __( "Language" ),
+            "label" => __( "Video Project Language" ),
             "labels" => $labels_video_project_language,
             "public" => true,
             "publicly_queryable" => true,
@@ -82,14 +76,16 @@ class VPS_Taxonomies_Initializer{
             "show_in_quick_edit" => false,
         );
 
-        register_taxonomy( 'country', array( "video_project" ), $args_country );
-        register_taxonomy( 'video_category', array( "video_project" ), $args_video_category );
+        register_taxonomy( 'video_project_country', array( "video_project" ), $args_country );
+        register_taxonomy( 'video_project_category', array( "video_project" ), $args_video_category );
         register_taxonomy( 'video_project_language', array( "video_project"), $args_video_project_language );
 
         //insert terms after taxonomies registered
+        /*
         if( !term_exists( 'english' )){
             wp_insert_term( 'English', 'video_project_language' );
         }
+        */
         
     }
 
