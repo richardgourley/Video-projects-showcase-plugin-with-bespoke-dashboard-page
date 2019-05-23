5ba0d6965b453fc15ca71dff3b9e6c6975ec1b00
unknown <codeleap@outlook.com>
2019-05-23 15:57:02 +0200

    Update video project class added, separating out logic conataining only update logic.

---
+++ classes-model/class-vps-update.php
@@ -0,0 +1,45 @@
+<?php
+
+class VPS_Update extends VPS_Model{
+    
+    protected $fields;
+    protected $helper;
+
+    public function __construct( $helper_class ){
+        //constructor logic
+        $this->helper = $helper_class;
+    }
+
+    public function update_video_project_form(){
+        require_once dirname( __DIR__ ) . '/admin-pages/update-video-project-form.php';
+    }
+
+    public function update_video_project(){
+        $message = $this->get_post_form_errors( $_POST, 'update' );
+
+        if(!$message == ''){
+            return $this->re_display_form( 
+                $message, 
+                $_POST, 
+                plugin_dir_path( __DIR__ ) . 'admin-pages/update-video-project-form.php' 
+            ); 
+        }
+
+        $this->sanitize_fields_assign( $_POST, 'update' ); //'update' means we have existing post ID
+
+        if($this->fields[ 'country' ] == 'other'){
+            $this->insert_new_country_term();
+
+            //assign new country term slug to country field.
+            $this->fields[ 'country' ] = 
+            get_term_by(
+                'name',
+                $this->helper->first_letter_upper( $this->fields[ 'new_country' ]), 
+                'country'
+            )->slug;
+        }
+
+        $this->create_or_update_post_assign_terms( 'update' );
+    }
+
+}
