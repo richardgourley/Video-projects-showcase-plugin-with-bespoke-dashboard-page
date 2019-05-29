4008c413a6c1da0d02a956cbd97c7356253095cf
unknown <codeleap@outlook.com>
2019-05-29 16:28:28 +0200

    Changed display layout and changed to display post meta not post content.

--- js/video-project-display.js
+++ js/video-project-display.js
@@ -7,19 +7,46 @@ var projectsDiv = document.getElementById("projectsDiv");
 
 console.log(videoProjects);
 
+function getVimeoId(vimeoLink){
+    vimeoLinkParts = vimeoLink.split("/");
+    return vimeoLinkParts[(vimeoLinkParts.length -1)];
+}
+
 function displayVideos(category){
 	if(videoProjects){
 		var output = '';
         for(i=0; i<videoProjects.length; i++){
         	if(videoProjects[i].category == category){
-        		output += '<div class="vps-col-5">';
+                output += '<div class="vps-div-border">';
+
+                output += '<div class="vps-row">';
                 output += '<h2>' + videoProjects[i].title + '</h2>';
-                output += videoProjects[i].content;
+                output += '</div>';
+
+                output += '<div class="vps-row">';
+
+                output += '<div class="vps-col-3">';
+                output += '<h3>Category: ' + videoProjects[i].category + '</h3>';
+                output += '<p>Location: ' + videoProjects[i].location + '</p>';
+                output += '<img src="' + videoProjects[i].image + '">';
+                output += '</div>';
+
+                output += '<div class="vps-col-8">';
+                output += '<p>Date: ' + videoProjects[i].date + '</p>';
+                output += '<p>Project Duration: ' + videoProjects[i].duration + '</p>';
+                output += '<iframe src="https://player.vimeo.com/video/';
+                output += getVimeoId(videoProjects[i].url);
+                output += '?color=fdfdfd" width="640" height="300" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+                output += '</div>';
+
+                output += '</div>';
                 output += '</div>';
         	}
         }
         if(output == ''){
-        	projectsDiv.innerHTML = 'No videos available in this category. Please check back later!';
+        	projectsDiv.innerHTML = '<br><h3>No videos available at the moment in the category: ' + category + '</h3>';
+            projectsDiv.innerHTML += '<p>Please check back later!</p>';
+            return;
         }
         projectsDiv.innerHTML = output;
 	}
