5984b8d581b45b27dd1d9b2c2161661261271e56
unknown <codeleap@outlook.com>
2019-05-30 22:16:50 +0200

    Activation hooks added to main file for custom post type registration and front end page creation.

--- video-project-showcase.php
+++ video-project-showcase.php
@@ -19,21 +19,28 @@ require dirname( __FILE__ ) . '/classes-init/class-vps-scripts-initializer.php';
 require dirname( __FILE__ ) . '/classes-init/class-vps-custom-post-type-initializer.php';
 require dirname( __FILE__ ) . '/classes-init/class-vps-taxonomies-initializer.php';
 require dirname( __FILE__ ) . '/classes-init/class-vps-page-initializer.php';
+require dirname( __FILE__ ) . '/classes-init/class-vps-localize-script.php';
 
-
-//initializes css and js scripts
+//registers css and js scripts on activation
 $vps_scripts_initializer = new VPS_Scripts_Initializer();
 
-//initializes custom post type
-$vps_custom_post_type_initializer = new VPS_Custom_Post_Type_Initializer();
-
-//initializes taxonomies
+//registers taxonomies on activation
 $vps_taxonomies_initializer = new VPS_Taxonomies_Initializer();
+
+//register custom post type and flush rewrites rules on activation
+$vps_custom_post_type_initializer = new VPS_Custom_Post_Type_Initializer();
+register_activation_hook( __FILE__, array( $vps_custom_post_type_initializer, 'register_post_type_activation' ));
 
 //initializes admin page in dashboard
 $vps_admin_page_initializer = new VPS_Admin_Page_Initializer();
 
-//initializes page and localizes script for the JS for the page.
+//creates a new page - only runs once on activation of plugin
 $vps_page_initializer = new VPS_Page_Initializer();
+register_activation_hook( __FILE__, array( $vps_page_initializer, 'create_video_project_page' ));
+
+/*
+//gets all video projects in php, then makes available as JS objects in 'video-project-display.js' file
+$vps_localize_script = new VPS_Localize_Script();
+*/
 
 
