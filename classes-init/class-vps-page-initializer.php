45ba0d42782e8f4e163b96d7782873689d13ebd4
unknown <codeleap@outlook.com>
2019-05-30 22:10:26 +0200

    Logic added to create project display page with content on plugin activation.

--- classes-init/class-vps-page-initializer.php
+++ classes-init/class-vps-page-initializer.php
@@ -2,12 +2,37 @@
 
 class VPS_Page_Initializer{
     public function __construct(){
-
+        
     }
     
     public function create_video_project_page(){
-        //code for custom page to be inserted into the site to display custom post type results - optional
+        //code for custom page to be inserted into the site to display custom post type results
+        $page_content = 
+            '<div class="vps-row">
+             <div class="vps-col-3">
+             <button id="commercialBtn">Commercial Projects</button>
+             </div>
+             <div class="vps-col-3">
+             <button id="musicVideoBtn">Music Videos</button>
+             </div>
+             <div class="vps-col-3">
+             <button id="weddingsBtn">Wedding projects</button>
+             </div>
+             </div>
+             <div class="vps-row" id="projectsDiv">
+             </div>';
+
+        $args = array(
+            'post_title'   => 'Video Projects',
+            'post_status'  => 'publish',
+            'post_content' => $page_content,
+            'post_type'    => 'page'
+        );
+
+        if( !post_exists( 'Video Projects' ) ){
+            wp_insert_post( $args );
+        }
+        
     }
 
-    
 }
