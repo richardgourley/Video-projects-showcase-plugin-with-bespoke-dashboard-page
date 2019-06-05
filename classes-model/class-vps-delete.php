712e27a32681c9cf7777e91ce77ba4b2e5747d95
unknown <codeleap@outlook.com>
2019-06-05 17:28:47 +0200

    Success message changed.

--- classes-model/class-vps-delete.php
+++ classes-model/class-vps-delete.php
@@ -26,37 +26,9 @@ class VPS_Delete extends VPS_Model{
         $result = wp_delete_post( $post_id, true );
         
         if($result){
-            echo "<h2>You have sucsessfully deleted the post titled: " . $result->post_title . "</h2>";
+            echo "<h2>You have successfully deleted the post titled: " . $result->post_title . "</h2>";
         }
 
-        /*
-        $message = $this->get_post_form_errors( $_POST, 'update' );
-
-        //message not blank means message contains FORM ERRORS - re-display form
-        if(!$message == ''){
-            return $this->re_display_form( 
-                $message, 
-                $_POST, 
-                plugin_dir_path( __DIR__ ) . 'admin-pages/update-video-project-form.php' 
-            ); 
-        }
-
-        $this->sanitize_fields_assign( 'update' ); //'update' means we have existing post ID
-
-        if($this->fields[ 'country' ] == 'other'){
-            $this->insert_new_country_term();
-
-            //assign new country term slug to country field.
-            $this->fields[ 'country' ] = 
-            get_term_by(
-                'name',
-                $this->helper->first_letter_upper( $this->fields[ 'new_country' ]), 
-                'video_project_country'
-            )->slug;
-        }
-
-        $this->create_or_update_post_assign_terms( 'update' );
-        */
     }
 
 }
