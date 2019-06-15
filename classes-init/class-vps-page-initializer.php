e6af049a4b3c79ff335e96a796c31c9bc401c8ed
unknown <codeleap@outlook.com>
2019-06-15 17:52:35 +0200

    Page initializer updated with page post_content containing a new div for displaying select boxes and video projects.

--- classes-init/class-vps-page-initializer.php
+++ classes-init/class-vps-page-initializer.php
@@ -8,18 +8,7 @@ class VPS_Page_Initializer{
     public function create_video_project_page(){
         //code for custom page to be inserted into the site to display custom post type results
         $page_content = 
-            '<div class="vps-row">
-             <div class="vps-col-3">
-             <button id="commercialBtn">Commercial Projects</button>
-             </div>
-             <div class="vps-col-3">
-             <button id="musicVideoBtn">Music Videos</button>
-             </div>
-             <div class="vps-col-3">
-             <button id="weddingsBtn">Wedding projects</button>
-             </div>
-             </div>
-             <div class="vps-row" id="projectsDiv">
+            '<div id="vpsProjectsContainer" class="vps-row">
              </div>';
 
         $args = array(
