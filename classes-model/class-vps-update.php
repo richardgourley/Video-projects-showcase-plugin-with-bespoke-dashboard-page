04f4339b94019ab77ae710fbd28af02fb039abb7
unknown <codeleap@outlook.com>
2019-06-26 15:20:01 +0200

    Insert terms logic moved from update file to sanitize_fields method in model file.

--- classes-model/class-vps-update.php
+++ classes-model/class-vps-update.php
@@ -28,18 +28,6 @@ class VPS_Update extends VPS_Model{
 
         $this->sanitize_fields_assign( 'update' ); //'update' means we have existing post ID
 
-        if($this->fields[ 'country' ] == 'other'){
-            $this->insert_new_country_term();
-
-            //assign new country term slug to country field.
-            $this->fields[ 'country' ] = 
-            get_term_by(
-                'name',
-                $this->helper->first_letter_upper( $this->fields[ 'new_country' ]), 
-                'video_project_country'
-            )->slug;
-        }
-
         $this->create_or_update_post_assign_terms( 'update' );
     }
 
