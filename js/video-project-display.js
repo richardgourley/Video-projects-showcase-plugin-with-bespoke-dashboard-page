bda1be83609d0e39c44dc4f029194e58e477b870
unknown <codeleap@outlook.com>
2019-07-02 21:18:06 +0200

    Comment added above div location check.

--- js/video-project-display.js
+++ js/video-project-display.js
@@ -10,19 +10,19 @@ if(document.getElementById("vpsProjectsContainer")){
     vpsProjectsContainer.innerHTML = pageSetUp.setDivs();
 
     //assign vars to divs set up in pageSetUp.setDivs()
-    let vpsSelectDiv = document.getElementById("vpsSelectDiv");
-    let vpsProjectDisplayDiv = document.getElementById("vpsProjectDisplayDiv");
+    let selectBoxDiv = document.getElementById("selectBoxDiv");
+    let projectDisplayDiv = document.getElementById("projectDisplayDiv");
 
     //populate selectDiv
-    vpsSelectDiv.innerHTML = pageSetUp.populateSelectBox();
+    selectBoxDiv.innerHTML = pageSetUp.populateSelectBox();
 
     //display videos for the first country
-    vpsProjectDisplayDiv.innerHTML = pageSetUp.displayProjects(countries[0]);
+    projectDisplayDiv.innerHTML = pageSetUp.displayProjects(countries[0]);
 
     //drop down menu on change
     countryDropDown.onchange = function(){
-        vpsProjectDisplayDiv.innerHTML = pageSetUp.displayProjects(
-            vpsCountryDropDown.options[vpsCountryDropDown.selectedIndex].value
+        projectDisplayDiv.innerHTML = pageSetUp.displayProjects(
+            countryDropDown.options[countryDropDown.selectedIndex].value
         );
     }
 
@@ -30,8 +30,8 @@ if(document.getElementById("vpsProjectsContainer")){
 
 function vpsSearchPageSetUp(){
     this.setDivs = function(){
-        let content = '<div id="vpsSelectDiv" class="vps-select-div">Testing</div>';
-        content += '<div id="vpsProjectDisplayDiv">Testing</div>';
+        let content = '<div id="selectBoxDiv" class="vps-select-div">Testing</div>';
+        content += '<div id="projectDisplayDiv">Testing</div>';
         return content;
     }
     this.populateSelectBox = function(){
@@ -41,7 +41,7 @@ function vpsSearchPageSetUp(){
         return content;
     }
     this.generateCountryDropDown = function(){
-        let dropDown = '<select class="vps-select-dropdown" id="vpsCountryDropDown">';
+        let dropDown = '<select class="vps-select-dropdown" id="countryDropDown">';
         dropDown += '<option>--Choose a country--</option>';
         for(i=0; i<countries.length; i++){
             dropDown += '<option value="'; 
