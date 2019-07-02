a2478c5dc5420180170e4747c3b97eb0b7354349
unknown <codeleap@outlook.com>
2019-07-02 16:44:58 +0200

    Functions added as methods with simpler names to PageSetUp class.

--- js/video-project-display.js
+++ js/video-project-display.js
@@ -1,6 +1,53 @@
 //only run this code on pages where vpsProjectsContainer is found
 function vpsSearchPageSetUp(){
-    
+    this.setDivs = function(){
+        let content = '<div id="vpsSelectDiv" class="vps-select-div">Testing</div>';
+        content += '<div id="vpsProjectDisplayDiv">Testing</div>';
+        return content;
+    }
+    this.populateSelectBoxes = function(){
+        let content = '';
+        content += '<p>Select a country to see projects</p>';
+        content += this.generateCountryDropDown();
+        return content;
+    }
+    this.generateCountryDropDown = function(){
+        let dropDown = '<select class="vps-select-dropdown" id="vpsCountryDropDown">';
+        dropDown += '<option>--Choose a country--</option>';
+        for(i=0; i<countries.length; i++){
+            dropDown += '<option value="'; 
+            dropDown += countries[i]; 
+            dropDown += '">';
+            dropDown += countries[i]; 
+            dropDown += '</option>';
+        }
+        dropDown += '</select>';
+        return dropDown;
+    }
+    this.displayProjects = function(country){
+        let content = '';
+        for(i=0; i<videoProjects.length; i++){
+            if(videoProjects[i].country == country){
+                content += '<div class="vps-grid">';
+                content += '<div class="vps-project-info">';
+                content += '<h2>' + videoProjects[i].title + '</h2>';
+                content += '<p class="vps-noto-sans-font">Category: ' + videoProjects[i].category + '</p>';
+                content += '<p class="vps-noto-sans-font">Location: ' + videoProjects[i].location;
+                content += ', ' + videoProjects[i].country + '</p>';
+                content += '<p class="vps-noto-sans-font">Date: ' + videoProjects[i].displaydate + '</p>';
+                content += '<p class="vps-noto-sans-font">Duration: ' + videoProjects[i].duration + '</p>';
+                content += '</div>'; //end project info
+                content += '<div class="vps-project-video">';
+                content += '<iframe class="vps-iframe" src="https://player.vimeo.com/video/';
+                content += videoProjects[i].videoid;
+                content += '?color=fdfdfd" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+
+                content += '</div>'; //end project video
+                content += '</div>'; //end grid
+            }
+        }
+        return content;
+    }
 }
 
 if(document.getElementById("vpsProjectsContainer")){
@@ -19,63 +66,6 @@ if(document.getElementById("vpsProjectsContainer")){
         vpsProjectDisplayDiv.innerHTML = vpsDisplayProjects(
             vpsCountryDropDown.options[vpsCountryDropDown.selectedIndex].value
         );
-    }
-
-    function vpsSetUpPage(){
-        let content = '<div id="vpsSelectDiv" class="vps-select-div">Testing</div>';
-        content += '<div id="vpsProjectDisplayDiv">Testing</div>';
-
-        return content;
-    }
-
-    function vpsPopulateSelectBox(){
-        let content = '';
-        content += '<p>Select a country to see projects</p>';
-        content += vpsGenerateCountryDropDown();
-
-        return content;
-    }
-
-    function vpsGenerateCountryDropDown(){
-        let dropDown = '<select class="vps-select-dropdown" id="vpsCountryDropDown">';
-        dropDown += '<option>--Choose a country--</option>';
-        for(i=0; i<countries.length; i++){
-            dropDown += '<option value="'; 
-            dropDown += countries[i]; 
-            dropDown += '">';
-            dropDown += countries[i]; 
-            dropDown += '</option>';
-        }
-        dropDown += '</select>';
-
-        return dropDown;
-    }
-
-    function vpsDisplayProjects(country){
-        let content = '';
-        for(i=0; i<videoProjects.length; i++){
-            if(videoProjects[i].country == country){
-                content += '<div class="vps-grid">';
-                content += '<div class="vps-project-info">';
-               // content += '<span class="vps-noto-sans-font-title">' + videoProjects[i].title + '</span>';
-                content += '<h2>' + videoProjects[i].title + '</h2>';
-                content += '<p class="vps-noto-sans-font">Category: ' + videoProjects[i].category + '</p>';
-                content += '<p class="vps-noto-sans-font">Location: ' + videoProjects[i].location;
-                content += ', ' + videoProjects[i].country + '</p>';
-                content += '<p class="vps-noto-sans-font">Date: ' + videoProjects[i].displaydate + '</p>';
-                content += '<p class="vps-noto-sans-font">Duration: ' + videoProjects[i].duration + '</p>';
-                content += '</div>'; //end project info
-                content += '<div class="vps-project-video">';
-                content += '<iframe class="vps-iframe" src="https://player.vimeo.com/video/';
-                content += videoProjects[i].videoid;
-                content += '?color=fdfdfd" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
-
-                content += '</div>'; //end project video
-                content += '</div>'; //end grid
-            }
-        }
-        
-        return content;
     }
 
 }
