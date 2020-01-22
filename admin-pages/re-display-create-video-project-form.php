8ac64f6417cf95e94170c3c84225801b32cafb64
unknown <codeleap@outlook.com>
2020-01-22 20:41:30 +0100

    Html indenting added to form

--- admin-pages/re-display-create-video-project-form.php
+++ admin-pages/re-display-create-video-project-form.php
@@ -1,19 +1,21 @@
 <div class="vps-main-page-div">
-<div>
+
+  <div>
     <p>There were errors in your form. Please check</p>
     <p><?php echo $message; ?></p>
-</div>
-<h1>Add a new video project</h1>
-<form action="<?php echo esc_url( admin_url('admin.php?page=video-project')); ?>" method="post">
-<?php wp_nonce_field( 'create-video-project-action', 'create-video-project-nonce' ); ?>
-<table class="form-table">
-    <tbody>
+  </div>
+
+  <h1>Add a new video project</h1>
+  <form action="<?php echo esc_url( admin_url('admin.php?page=video-project')); ?>" method="post">
+    <?php wp_nonce_field( 'create-video-project-action', 'create-video-project-nonce' ); ?>
+    <table class="form-table">
+      <tbody>
     	<tr>
-        	<th>
-        		<label for="category">Select a category for this video</label>
-        	</th>
-        	<td>
-                <?php
+          <th>
+            <label for="category">Select a category for this video</label>
+          </th>
+          <td>
+            <?php
                 $video_project_category_terms = get_terms( array(
                     'taxonomy' =>   'video_project_category',
                     'hide_empty' => false
@@ -33,135 +35,134 @@
                         esc_html( $term->slug ),
                         esc_html( $term->name )
                     );
-
                 }
-                ?>
-        	</td>
+            ?>
+          </td>
         </tr>
         <tr>
-        	<th>
-        		<label for="country">Select a country where the video was filmed</label>
-        	</th>
-        	<td>
-        		<div id="countries">
-                <?php
-                $video_project_country_terms = get_terms( array(
-                    'taxonomy' =>   'video_project_country',
-                    'hide_empty' => false
-                ));
+          <th>
+            <label for="country">Select a country where the video was filmed</label>
+          </th>
+          <td>
+        	<div id="countries">
+              <?php
+              $video_project_country_terms = get_terms( array(
+                  'taxonomy' =>   'video_project_country',
+                  'hide_empty' => false
+              ));
                 
-                //set to true unless we find existing country below
-                $new_country = true;
+              //set to true unless we find existing country below
+              $new_country = true;
 
-                foreach($video_project_country_terms as $country_term){
+              foreach($video_project_country_terms as $country_term){
                     
-                    if($country_term->slug == $post['country']){
-                        //existing country selected - NOT a new country
-                        $new_country = false;
-                        //prints existing country with checked box
-                        printf('<label><input checked type="radio" id=%s name="country" value=%s>%s</label> ',
-                            esc_html( $country_term->slug),
-                            esc_html( $country_term->slug),
-                            esc_html( $country_term->name)
-                        );
-                    }else{
-                        //prints existing country not checked
-                        printf('<label><input type="radio" id=%s name="country" value=%s>%s</label> ',
-                            esc_html( $country_term->slug),
-                            esc_html( $country_term->slug),
-                            esc_html( $country_term->name)
-                        );
-                    }
-                }
+                  if($country_term->slug == $post['country']){
+                      //existing country selected - NOT a new country
+                      $new_country = false;
+                      //prints existing country with checked box
+                      printf('<label><input checked type="radio" id=%s name="country" value=%s>%s</label> ',
+                          esc_html( $country_term->slug),
+                          esc_html( $country_term->slug),
+                          esc_html( $country_term->name)
+                      );
+                  }else{
+                      //prints existing country not checked
+                      printf('<label><input type="radio" id=%s name="country" value=%s>%s</label> ',
+                          esc_html( $country_term->slug),
+                          esc_html( $country_term->slug),
+                          esc_html( $country_term->name)
+                      );
+                  }
+              }
 
-                //other is checked or not and value printed or not depending if $new_country is true
-                if($new_country){
-                    $country = $post['new-country'];
-                    echo
-                        '<br /><br /><label><input checked type="radio" id="other" name="country" value="other">Other</label>
-                        <input type="text" id="new-country" name="new-country" value="' . $country .'">';
-                }else{
-                    printf(
-                        '<br /><br /><label><input type="radio" id="other" name="country" value="other">Other</label>
-                        <input placeholder="Enter country" type="text" id="new-country" name="new-country" value="">'
-                    ); 
-                }
+              //other is checked or not and value printed or not depending if $new_country is true
+              if($new_country){
+                  $country = $post['new-country'];
+                  echo
+                      '<br /><br /><label><input checked type="radio" id="other" name="country" value="other">Other</label>
+                      <input type="text" id="new-country" name="new-country" value="' . $country .'">';
+              }else{
+                  printf(
+                      '<br /><br /><label><input type="radio" id="other" name="country" value="other">Other</label>
+                       <input placeholder="Enter country" type="text" id="new-country" name="new-country" value="">'
+                  ); 
+              }
 
-                ?>
-                </div>
-                <br />
+              ?>
+            </div>
+            <br />
                 
-        	</td>
+          </td>
         </tr>
         <tr>
-        	<th>
-        		<label for="title">Title</label>
-        		<p><small>Enter the project name or the name of the customer or client.</small></p>
-        	</th>
-        	<td>
-        		<input class="regular-text" id="title" name="title" type="text" value="<?php echo $post['title']; ?>">
-        	</td>
+          <th>
+        	<label for="title">Title</label>
+        	  <p><small>Enter the project name or the name of the customer or client.</small></p>
+          </th>
+          <td>
+        	<input class="regular-text" id="title" name="title" type="text" value="<?php echo $post['title']; ?>">
+          </td>
         </tr>
         <tr>
-        	<th>
-        		<label for="location">Location</label>
-        		<p><small>Enter the town or city of the project.</small></p>
-        	</th>
-        	<td>
-        		<input class="regular-text" id="location" name="location" type="text" 
+          <th>
+        	<label for="location">Location</label>
+        	<p><small>Enter the town or city of the project.</small></p>
+          </th>
+          <td>
+        	<input class="regular-text" id="location" name="location" type="text" 
                 value="<?php echo $post['location']; ?>">
         	</td>
         </tr>
         <tr>
-        	<th>
-        		<label for="date">Date</label>
-        		<p><small>Select a date for the project.</small></p>
-        	</th>
-        	<td>
-        		<input class="regular-text" id="date" name="date" type="date" value="<?php echo $post['date']; ?>">
-        	</td>
+          <th>
+        	<label for="date">Date</label>
+        	<p><small>Select a date for the project.</small></p>
+          </th>
+          <td>
+        	<input class="regular-text" id="date" name="date" type="date" value="<?php echo $post['date']; ?>">
+          </td>
         </tr>
         <tr>
-        	<th>
-        		<label for="duration">Duration</label>
-        		<p><small>Enter a duration eg 2 weeks, 3 months.</small></p>
-        	</th>
-        	<td>
-        		<input class="regular-text" id="duration" name="duration" type="text" 
+          <th>
+        	<label for="duration">Duration</label>
+        	<p><small>Enter a duration eg 2 weeks, 3 months.</small></p>
+          </th>
+          <td>
+        	<input class="regular-text" id="duration" name="duration" type="text" 
                 value="<?php echo $post['duration']; ?>">
         	</td>
         </tr>
         <tr>
-        	<th>
-        		<label for="image-url">Image</label>
-        		<p><small>Click the upload button to choose an image from the media library.</small></p>
-        	</th>
-        	<td>
-        		<input class="regular-text" id="image-url" name="image-url" type="text" 
+          <th>
+        	<label for="image-url">Image</label>
+        	<p><small>Click the upload button to choose an image from the media library.</small></p>
+          </th>
+          <td>
+        	<input class="regular-text" id="image-url" name="image-url" type="text" 
                 value="<?php echo $post['image-url']; ?>">
-        		<input class="button insert-video-project-form_button" id="upload-image-video-project" name="video-project-button" type="button" value="Upload"/>
-        	</td>
+        	<input class="button insert-video-project-form_button" id="upload-image-video-project" name="video-project-button" type="button" value="Upload"/>
+          </td>
         </tr>
         <tr>
-        	<th>
-        		<label for="video-url">Video Url</label>
-        		<p><small>Enter the url of the video for this project (Vimeo or youtube link etc.)</small></p>
-        	</th>
-        	<td>
-        		<input class="regular-text" id="video-url" name="video-url" type="text" 
+          <th>
+        	<label for="video-url">Video Url</label>
+        	<p><small>Enter the url of the video for this project (Vimeo or youtube link etc.)</small></p>
+          </th>
+          <td>
+        	<input class="regular-text" id="video-url" name="video-url" type="text" 
                 value="<?php echo $post['video-url']; ?>">
         	</td>
         </tr>
         <tr>
-        	<th>
-        		<label for="submit">Add new Video Project</label>
-        	</th>
-        	<td>
-        		<input type="submit" value="Submit">
-        	</td>
+          <th>
+        	<label for="submit">Add new Video Project</label>
+          </th>
+          <td>
+        	<input type="submit" value="Submit">
+          </td>
         </tr>
-    </tbody>
-</table>
-</form> <!---End form--->
+      </tbody>
+    </table>
+  </form> <!---End form--->
 
 </div><!---End main div--->
