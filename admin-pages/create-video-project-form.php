a2e330f22d200a56976b03e40f86bc3c9affebf7
unknown <codeleap@outlook.com>
2020-01-22 20:15:12 +0100

    Html indenting added to second part of form table.

--- admin-pages/create-video-project-form.php
+++ admin-pages/create-video-project-form.php
@@ -49,78 +49,80 @@
             <?php endforeach ?>
           </div>
             	    
-                    <br />
-                    <br />
-                    <label><input type="radio" id="other" name="country" value="other">Other</label>
-                    <input placeholder="Enter country" type="text" id="new-country" name="new-country" 
-                    value="">
-            	</td>
-            </tr>
-            <tr>
-            	<th>
-            		<label for="title">Title</label>
-            		<p><small>Enter the project name or the name of the customer or client.</small></p>
-            	</th>
-            	<td>
-            		<input class="regular-text" id="title" name="title" type="text" value="">
-            	</td>
-            </tr>
-            <tr>
-            	<th>
-            		<label for="location">Location</label>
-            		<p><small>Enter the town or city of the project.</small></p>
-            	</th>
-            	<td>
-            		<input class="regular-text" id="location" name="location" type="text" value="">
-            	</td>
-            </tr>
-            <tr>
-            	<th>
-            		<label for="date">Date</label>
-            		<p><small>Select a date for the project.</small></p>
-            	</th>
-            	<td>
-            		<input class="regular-text" id="date" name="date" type="date" value="">
-            	</td>
-            </tr>
-            <tr>
-            	<th>
-            		<label for="duration">Duration</label>
-            		<p><small>Enter a duration for the project - 2 weeks, 3 months etc.</small></p>
-            	</th>
-            	<td>
-            		<input class="regular-text" id="duration" name="duration" type="text" value="">
-            	</td>
-            </tr>
-            <tr>
-            	<th>
-            		<label for="image-url">Image</label>
-            		<p><small>Click the upload button to choose an image from the media library.</small></p>
-            	</th>
-            	<td>
-            		<input class="regular-text" id="image-url" name="image-url" type="text" value="">
-            		<input class="button insert-video-project-form_button" id="upload-image-video-project" name="video-project-button" type="button" value="Upload"/>
-            	</td>
-            </tr>
-            <tr>
-            	<th>
-            		<label for="video-url">Video Url</label>
-            		<p><small>Enter the url of the video for this project (Vimeo or youtube link etc.)</small></p>
-            	</th>
-            	<td>
-            		<input class="regular-text" id="video-url" name="video-url" type="text" value="">
-            	</td>
-            </tr>
-            <tr>
-            	<th>
-            		<label for="submit">Add new Video Project</label>
-            	</th>
-            	<td>
-            		<input type="submit" value="Submit">
-            	</td>
-            </tr>
-        </tbody>
-    </table>
+          <br />
+          <br />
+          <label>
+            <input type="radio" id="other" name="country" value="other">Other
+          </label>
+          <input placeholder="Enter country" type="text" id="new-country" name="new-country" 
+          value="">
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label for="title">Title</label>
+          <p><small>Enter the project name or the name of the customer or client.</small></p>
+        </th>
+        <td>
+          <input class="regular-text" id="title" name="title" type="text" value="">
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label for="location">Location</label>
+          <p><small>Enter the town or city of the project.</small></p>
+        </th>
+        <td>
+          <input class="regular-text" id="location" name="location" type="text" value="">
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label for="date">Date</label>
+          <p><small>Select a date for the project.</small></p>
+        </th>
+        <td>
+          <input class="regular-text" id="date" name="date" type="date" value="">
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label for="duration">Duration</label>
+          <p><small>Enter a duration for the project - 2 weeks, 3 months etc.</small></p>
+        </th>
+        <td>
+          <input class="regular-text" id="duration" name="duration" type="text" value="">
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label for="image-url">Image</label>
+          <p><small>Click the upload button to choose an image from the media library.</small></p>
+        </th>
+        <td>
+          <input class="regular-text" id="image-url" name="image-url" type="text" value="">
+          <input class="button insert-video-project-form_button" id="upload-image-video-project" name="video-project-button" type="button" value="Upload"/>
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label for="video-url">Video Url</label>
+          <p><small>Enter the url of the video for this project (Vimeo or youtube link etc.)</small></p>
+        </th>
+        <td>
+          <input class="regular-text" id="video-url" name="video-url" type="text" value="">
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <label for="submit">Add new Video Project</label>
+        </th>
+        <td>
+          <input type="submit" value="Submit">
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </form> <!---End form--->
 
 </div><!---End main div--->
